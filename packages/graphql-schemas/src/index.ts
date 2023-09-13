dd429834439fd4d730e1816446e6550848e1d3e5
Jamie Sunderland <jamesrainersunderland@gmail.com>
2023-09-12 17:55:31 -0700

    [jrs] fixed graphql gen

--- packages/graphql-schemas/src/index.ts
+++ packages/graphql-schemas/src/index.ts
@@ -5,4 +5,4 @@ export const mainSchema = main;
 export const adminSchema = admin;
 
 export * as main from './generated/main-graphql';
-//export * as admin from './generated/admin-graphql';
+export * as admin from './generated/admin-graphql';
