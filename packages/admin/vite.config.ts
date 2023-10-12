6e903103d920a562aa5a49934f7c4fac3431fde3
Jamie Sunderland <jamesrainersunderland@gmail.com>
2023-10-11 21:24:21 -0700

    [jrs] added fixes

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,26 +1,37 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }): any => {
+export default defineConfig(({ command, mode }): any => {
+  const cdnHost = process.env?.CDN_HOST ?? "";
   return {
     mode: process.env.MODE,
     outDir: 'dist',
     plugins: [react()],
+    publicDir: 'public',
     assetsInclude: [
-      '@floro/common-assets/assets/**/*.svg',
-      '@floro/common-assets/assets/**/*.png',
-      '@floro/common-assets/assets/**/*.ttf',
-      '@floro/common-assets/assets/**/*.txt',
+      '../common-assets/assets/**/*.svg',
+      '../common-assets/assets/images/**/*.svg',
+      '../common-assets/assets/images/icons/**/*.svg',
+      '../common-assets/assets/images/repo_icons/**/*.svg',
+      '../common-assets/assets/images/rich_text_icons/**/*.svg',
+      '../common-assets/assets/**/*.png',
+      '../common-assets/assets/**/*.txt',
+      '../common-assets/assets/**/*.ttf',
     ],
-    build: {
-      minify: mode === 'production',
-    },
     server: {
       hmr: {
         clientPort: 7778,
         port: 7778,
       }
-    }
+    },
+    experimental: {
+      renderBuiltUrl: (filename: string, { hostId }) => {
+        if (command == "build" && cdnHost != "") {
+          return `${cdnHost}/${filename}`;
+        }
+        return { relative: true };
+      },
+    },
   }
 });
